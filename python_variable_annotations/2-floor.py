96ea21bf0466633bde250d4d97d5aa3dc8a722cd
seliisyl <8859@holbertonstudents.com>
2024-08-17 01:11:45 +0200

    Variable Annotations

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to get the lower integer
+part of a floating point number.
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    return math.floor(n)
