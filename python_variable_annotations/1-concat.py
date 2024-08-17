bea990071aa838669ea09220680541bb736763b8
seliisyl <8859@holbertonstudents.com>
2024-08-17 21:28:42 +0200

    Variable Annotations

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,4 +5,18 @@ This module provides a function to concatenate two strings.
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Concatène deux chaînes de caractères et retourne la chaîne résultante.
+
+    Paramètres :
+    str1 (str) : La première chaîne de caractères.
+    str2 (str) : La deuxième chaîne de caractères.
+
+    Retourne :
+    str : La chaîne résultante de la concaténation de str1 et str2.
+
+    Exemple :
+    >>> concat("hello", "world")
+    'helloworld'
+    """
     return str1 + str2
