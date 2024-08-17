7fbec4c8a86ed78fe269ed2c0c2fd81ff7ccd6d3
seliisyl <8859@holbertonstudents.com>
2024-08-17 02:30:27 +0200

    Variable Annotations

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to calculate
+the sum of the elements
+of a list of floating-point numbers.
+"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    return sum(input_list)
