b06a556c8147f2bc0cbbc3fa7febfcbbf7cffbc3
seliisyl <8859@holbertonstudents.com>
2024-08-18 17:37:01 +0200

    Variable Annotations

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-This module provides a function to create a tuple
-from a string and a number,
-where the number is squared.
+Ce module fournit une fonction pour créer un tuple à partir
+d'une chaîne de caractères et d'un nombre, où le nombre est
+au carré.
 """
 
 
@@ -10,4 +10,22 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Prend une chaîne de caractères et un nombre (entier ou flottant),
+    et retourne un tuple où le nombre est au carré.
+
+    Paramètres :
+    k (str): La chaîne de caractères.
+    v (Union[int, float]): Le nombre à mettre au carré.
+
+    Retourne :
+    Tuple[str, float]: Un tuple avec la chaîne de caractères
+    et le carré du nombre.
+
+    Exemple :
+    >>> to_kv("eggs", 3)
+    ('eggs', 9.0)
+    >>> to_kv("school", 0.02)
+    ('school', 0.0004)
+    """
     return (k, float(v ** 2))
