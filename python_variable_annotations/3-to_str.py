06537e00dff1fed0aed65aee2be3b65660730041
seliisyl <8859@holbertonstudents.com>
2024-08-18 17:12:02 +0200

    Variable Annotations

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -6,4 +6,17 @@ of a floating point number.
 
 
 def to_str(n: float) -> str:
+    """
+    Convertit un nombre flottant en une chaîne de caractères.
+
+    Paramètre :
+    n (float) : Le nombre flottant à convertir en chaîne de caractères.
+
+    Retourne :
+    str : La représentation en chaîne de caractères du nombre flottant.
+
+    Exemple :
+    >>> to_str(3.14)
+    '3.14'
+    """
     return str(n)
