50abd7d58ac14659e816ff07acbceb1911036252
seliisyl <8859@holbertonstudents.com>
2024-08-18 17:30:58 +0200

    Variable Annotations

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
 This module provides a function to calculate
-the sum of the elements
-of a list of integers and floating-point numbers.
+the sum of the elements of a list of integers and floating-point numbers.
 """
 
 
@@ -10,4 +9,18 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Calcule la somme des nombres entiers et flottants dans une liste.
+
+    Paramètre :
+    mxd_lst (List[Union[int, float]]): Une liste de nombres entiers
+    et flottants à additionner.
+
+    Retourne :
+    float: La somme des nombres entiers et flottants dans la liste.
+
+    Exemple :
+    >>> sum_mxd_lst([5, 4, 3.14, 666, 0.99])
+    679.13
+    """
     return sum(mxd_lst)
