7043d9f4670f3a6866622047ce5bb184922c5525
seliisyl <8859@holbertonstudents.com>
2024-08-18 17:04:29 +0200

    Variable Annotations

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -9,4 +9,18 @@ import math
 
 
 def floor(n: float) -> int:
+    """
+    Retourne la partie entière inférieure du nombre flottant donné.
+
+    Paramètre :
+    n (float) : Le nombre flottant dont on veut obtenir
+    la partie entière inférieure.
+
+    Retourne :
+    int : La partie entière inférieure de n.
+
+    Exemple :
+    >>> floor(3.14)
+    3
+    """
     return math.floor(n)
